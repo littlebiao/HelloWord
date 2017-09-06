b4c4b777f914ebb5c72b451be0a7dd6e22428b80
G B <gengbiao@blueseafintech.com>
2017-09-06 10:56:52 +0800

    ssss

--- serverce.js
+++ serverce.js
@@ -2,6 +2,15 @@ var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/', secret: 'bluesea' })
  
+function run_cmd(cmd, args, callback) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var resp = "";
+
+  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
+  child.stdout.on('end', function() { callback (resp) });
+}
+
 http.createServer(function (req, res) {
 	console.log("req")
   handler(req, res, function (err) {
@@ -18,6 +27,8 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
+	run_cmd('sh', ['./shell.sh',event.payload.repository.name], function(text){ console.log(text) });
+
 })
  
 handler.on('issues', function (event) {
@@ -27,3 +38,4 @@ handler.on('issues', function (event) {
     event.payload.issue.number,
     event.payload.issue.title)
 })
+
