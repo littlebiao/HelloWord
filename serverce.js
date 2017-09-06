024e063230ff2316140aee74f810555adff66ba0
G B <gengbiao@blueseafintech.com>
2017-09-06 11:36:05 +0800

    add

--- serverce.js
+++ serverce.js
@@ -12,7 +12,6 @@ function run_cmd(cmd, args, callback) {
 }
 
 http.createServer(function (req, res) {
-	console.log("req")
   handler(req, res, function (err) {
     res.statusCode = 404
     res.end('no such location')
